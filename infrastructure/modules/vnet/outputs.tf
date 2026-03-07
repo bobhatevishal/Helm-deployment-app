0f6b2d0aa3c3f6213eceb8aab841fa8daa7fcffa
Bobhate Vishal <bobhatev13@gmail.com>
2026-03-07 18:22:28 +0530

    Refactor outputs in outputs.tf for clarity

--- infrastructure/modules/vnet/outputs.tf
+++ infrastructure/modules/vnet/outputs.tf
@@ -1,15 +1,12 @@
 output "vnet_id" {
-  description = "ID of the Virtual Network"
   value       = azurerm_virtual_network.vnet.id
 }
 
 output "vnet_name" {
-  description = "Name of the Virtual Network"
   value       = azurerm_virtual_network.vnet.name
 }
 
 output "private_subnet_ids" {
-  description = "List of private subnet IDs"
   value = [
     azurerm_subnet.private["pvt-subnet-0"].id,
     azurerm_subnet.private["pvt-subnet-1"].id,
@@ -19,7 +16,6 @@ output "private_subnet_ids" {
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
   value = [
     azurerm_subnet.public["pub-subnet-0"].id,
     azurerm_subnet.public["pub-subnet-1"].id
@@ -27,6 +23,5 @@ output "public_subnet_ids" {
 }
 
 output "nsg_id" {
-  description = "ID of the Network Security Group"
   value       = azurerm_network_security_group.nsg.id
 }
