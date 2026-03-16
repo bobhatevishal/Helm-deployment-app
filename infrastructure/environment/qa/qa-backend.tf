a495cef1062f85bc11c738ff0838ef927839cc90
bobhatevishal <bobhatev13@gmail.com.com>
2026-03-16 08:13:15 +0000

    update file stracture

--- infrastructure/environment/qa/qa-backend.tf
+++ infrastructure/environment/qa/qa-backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "snaplinktfstate99"
     container_name       = "tfstate"
-    key                  = "qa/dev.tfstate"
+    key                  = "qa/qa.tfstate"
   }
 }
