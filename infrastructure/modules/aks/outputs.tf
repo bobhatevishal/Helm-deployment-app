b555d4080fb1843b555bea49d680be59d86662fe
bobhatevishal <bobhatev13@gmail.com.com>
2026-03-17 16:57:27 +0000

    ArgoCD files added

--- infrastructure/modules/aks/outputs.tf
+++ infrastructure/modules/aks/outputs.tf
@@ -7,7 +7,7 @@ output "cluster_id" {
 }
 
 output "kube_config" {
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value       = azurerm_kubernetes_cluster.aks.kube_config
   sensitive   = true
 }
 
